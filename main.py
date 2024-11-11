f530d09fc431a404fc8d0926769d0f45802bfe2c
Dich3x <aleksprox9@gmail.com>
2024-11-11 16:57:19 +0300

    4: add count sharacters

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     print(text)
     len_tex = split_text(text)
     print(f"words in text {len_tex}")
+    print("")
+    print(dict_of_symbols(text))
 
 def get_books_text(path):
     with open(path) as f:
@@ -13,4 +15,14 @@ def get_books_text(path):
 def split_text(text):
     words = text.split()
     return(len(words))
+
+def dict_of_symbols(text):
+    new_dict = {}
+    for symbol in list(text.lower()):
+        if (symbol in new_dict):
+             new_dict[symbol] += 1
+        else:
+            new_dict[symbol] = 1
+    return new_dict
+    
 main()
